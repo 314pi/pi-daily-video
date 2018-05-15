fe2251d8d39950e2495a06553805ebeef3596e22
Pilikeyou <36942263+314pi@users.noreply.github.com>
2018-05-15 22:56:16 +0700

    some changes

--- record/vtv1.bat
+++ record/vtv1.bat
@@ -1,3 +1,3 @@
 set filename=%date:~0,2%%date:~3,2%%date:~6,4%-%time:~0,2%%time:~3,2%%time:~6,2%
 echo %filename%
-start "vtv1" streamlink --player "C:\Program Files (x86)\VideoLAN\VLC\vlc.exe -I dummy --sout=file/ts:%filename%.ts" http://vtvgo-live-appobj.b5695cde.cdnviet.com/fe96a8880d5a70ad3e08d93dbd1cc3ca1526399426/live/_definst_/vtv1-mid.m3u8 worst --hls-segment-threads 3
+start "vtv1" streamlink --player "C:\Program Files (x86)\VideoLAN\VLC\vlc.exe --sout=file/ts:%filename%.ts --run-time 5400" http://vtvgo-live-appobj.b5695cde.cdnviet.com/fe96a8880d5a70ad3e08d93dbd1cc3ca1526399426/live/_definst_/vtv1-mid.m3u8 worst --hls-segment-threads 3
