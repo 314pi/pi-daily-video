427d7f306d573f697b749537bed66e9209618fdc
Pilikeyou <36942263+314pi@users.noreply.github.com>
2018-05-16 18:05:02 +0700

    --network-caching=60000

--- record/thvl1.bat
+++ record/thvl1.bat
@@ -1,5 +1,5 @@
 set filename=thvl1_%date:~0,2%%date:~3,2%_%time:~0,2%%time:~3,2%%time:~6,2%
-set vlc=C:\Program Files (x86)\VideoLAN\VLC\vlc.exe -I dummy --sout=file/ts:%filename%.ts --run-time 4200 --play-and-exit
+set vlc=C:\Program Files (x86)\VideoLAN\VLC\vlc.exe -I dummy --sout=file/ts:%filename%.ts --network-caching=60000 --run-time 4200 --play-and-exit
 
 set thvl1=http://talk2.vcdn.vn/hls/9068ed7d7c3a6661a8e31fbfd4498ea5/1636896d974/thoixungan/index.m3u8
 
