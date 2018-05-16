977baed8be63ddd38ac591ce5b71bf37f65fbf9e
Pilikeyou <36942263+314pi@users.noreply.github.com>
2018-05-16 17:57:30 +0700

    -I dummy

--- record/thvl1.bat
+++ record/thvl1.bat
@@ -1,6 +1,6 @@
 set filename=thvl1_%date:~0,2%%date:~3,2%_%time:~0,2%%time:~3,2%%time:~6,2%
-set vlc=C:\Program Files (x86)\VideoLAN\VLC\vlc.exe --sout=file/ts:%filename%.ts --run-time 4200 --play-and-exit
+set vlc=C:\Program Files (x86)\VideoLAN\VLC\vlc.exe -I dummy --sout=file/ts:%filename%.ts --run-time 4200 --play-and-exit
 
-set thvl1=http://talk2.vcdn.vn/hls/7e72e4ec5b74976727ff432af3a1dbe6/1636716a569/thoixungan/index.m3u8
+set thvl1=http://talk2.vcdn.vn/hls/9068ed7d7c3a6661a8e31fbfd4498ea5/1636896d974/thoixungan/index.m3u8
 
 start "thvl1" streamlink --player "%vlc%" %thvl1% worst --hls-segment-threads 3
