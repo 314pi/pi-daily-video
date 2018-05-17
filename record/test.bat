fa4afc3af0d1f9139f7055783fa2baf37a9fbace
Pilikeyou <36942263+314pi@users.noreply.github.com>
2018-05-17 08:29:05 +0700

    them ham kiem tra thoi gian thoat

--- record/test.bat
+++ record/test.bat
@@ -1,6 +1,57 @@
-@echo off 
-set str=This string    has    a  lot  of spaces 
-echo %str% 
+@echo off
+    setlocal enableextensions disabledelayedexpansion
 
-set str=%str: =%
-echo %str%
+    call :getTime now
+
+    set "task=day"
+    if "%now%" lss "03:30:00,00" ( set "task=night" ) 
+    if "%now%" geq "22:00:00,00" ( set "task=night" )
+
+    call :task_%task%
+
+    echo %now%
+
+    endlocal
+    exit /b
+
+:task_day
+    :: do daily task
+    goto :eof
+
+:task_night
+    :: do nightly task
+    goto :eof
+
+:: getTime
+::    This routine returns the current (or passed as argument) time
+::    in the form hh:mm:ss,cc in 24h format, with two digits in each
+::    of the segments, 0 prefixed where needed.
+:getTime returnVar [time]
+    setlocal enableextensions disabledelayedexpansion
+
+    :: Retrieve parameters if present. Else take current time
+    if "%~2"=="" ( set "t=%time%" ) else ( set "t=%~2" )
+
+    :: Test if time contains "correct" (usual) data. Else try something else
+    echo(%t%|findstr /i /r /x /c:"[0-9:,.apm -]*" >nul || ( 
+        set "t="
+        for /f "tokens=2" %%a in ('2^>nul robocopy "|" . /njh') do (
+            if not defined t set "t=%%a,00"
+        )
+        rem If we do not have a valid time string, leave
+        if not defined t exit /b
+    )
+
+    :: Check if 24h time adjust is needed
+    if not "%t:pm=%"=="%t%" (set "p=12" ) else (set "p=0")
+
+    :: Separate the elements of the time string
+    for /f "tokens=1-5 delims=:.,-PpAaMm " %%a in ("%t%") do (
+        set "h=%%a" & set "m=00%%b" & set "s=00%%c" & set "c=00%%d" 
+    )
+
+    :: Adjust the hour part of the time string
+    set /a "h=100%h%+%p%"
+
+    :: Clean up and return the new time string
+    endlocal & if not "%~1"=="" set "%~1=%h:~-2%:%m:~-2%:%s:~-2%,%c:~-2%" & exit /b
--- record/thvl1.bat
+++ record/thvl1.bat
@@ -1,3 +1,6 @@
+@echo off
+setlocal enableextensions disabledelayedexpansion
+
 set filename=thvl1_%date:~0,2%%date:~3,2%_%time:~0,2%%time:~3,2%%time:~6,2%.ts
 set filename=%filename: =% 
 set vlc=C:\Program Files (x86)\VideoLAN\VLC\vlc.exe -I dummy --sout=file/ts:%filename% --network-caching=60000 --run-time 4200 --play-and-exit
@@ -7,7 +10,42 @@ set thvl1=http://talk2.vcdn.vn/hls/c056383b080b512d0629dc43b39c018d/1636b77954c/
 :start_record
 
 tasklist /fi "WindowTitle eq pi-thvl1" | find /i "streamlink.exe" || start "pi-thvl1" streamlink --player "%vlc%" %thvl1% worst --hls-segment-threads 3
-
 timeout /t 10 /nobreak
+call :getTime now
+if "%now%" geq "13:05:00,00" ( goto :eof )
 
 goto start_record
+
+:: getTime
+::    This routine returns the current (or passed as argument) time
+::    in the form hh:mm:ss,cc in 24h format, with two digits in each
+::    of the segments, 0 prefixed where needed.
+:getTime returnVar [time]
+    setlocal enableextensions disabledelayedexpansion
+
+    :: Retrieve parameters if present. Else take current time
+    if "%~2"=="" ( set "t=%time%" ) else ( set "t=%~2" )
+
+    :: Test if time contains "correct" (usual) data. Else try something else
+    echo(%t%|findstr /i /r /x /c:"[0-9:,.apm -]*" >nul || ( 
+        set "t="
+        for /f "tokens=2" %%a in ('2^>nul robocopy "|" . /njh') do (
+            if not defined t set "t=%%a,00"
+        )
+        rem If we do not have a valid time string, leave
+        if not defined t exit /b
+    )
+
+    :: Check if 24h time adjust is needed
+    if not "%t:pm=%"=="%t%" (set "p=12" ) else (set "p=0")
+
+    :: Separate the elements of the time string
+    for /f "tokens=1-5 delims=:.,-PpAaMm " %%a in ("%t%") do (
+        set "h=%%a" & set "m=00%%b" & set "s=00%%c" & set "c=00%%d" 
+    )
+
+    :: Adjust the hour part of the time string
+    set /a "h=100%h%+%p%"
+
+    :: Clean up and return the new time string
+    endlocal & if not "%~1"=="" set "%~1=%h:~-2%:%m:~-2%:%s:~-2%,%c:~-2%" & exit /b
