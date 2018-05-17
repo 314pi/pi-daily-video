467dd4e235ef0aa9919f7235ea1d7e3f27c790f9
Pilikeyou <36942263+314pi@users.noreply.github.com>
2018-05-17 11:59:14 +0700

    some changes

--- record/thvl1.bat
+++ record/thvl1.bat
@@ -5,11 +5,22 @@ set filename=thvl1_%date:~0,2%%date:~3,2%_%time:~0,2%%time:~3,2%%time:~6,2%.ts
 set filename=%filename: =% 
 set vlc=C:\Program Files (x86)\VideoLAN\VLC\vlc.exe -I dummy --sout=file/ts:%filename% --network-caching=60000 --run-time 4200 --play-and-exit
 
-set thvl1=http://talk2.vcdn.vn/hls/c056383b080b512d0629dc43b39c018d/1636b77954c/thoixungan/index.m3u8
+set thvl1=http://talk2.vcdn.vn/hls/cc9249254ad58a94b6ad1734795192b2/1636c290824/thoixungan/index.m3u8
 
 :start_record
 
-tasklist /fi "WindowTitle eq pi-thvl1" | find /i "streamlink.exe" || start "pi-thvl1" streamlink --player "%vlc%" %thvl1% worst --hls-segment-threads 3
+tasklist /fi "WindowTitle eq pi-thvl1" | find /i "streamlink.exe" || (
+:: check if url is ready.
+streamlink %thvl1% | find /i "Available streams" || (
+:beep
+echo Kiem Tra Lai Link !
+rundll32 user32.dll,MessageBeep
+@timeout /t 3
+goto beep
+)
+start "pi-thvl1" streamlink --player "%vlc%" %thvl1% worst --hls-segment-threads 3
+)
+
 timeout /t 10 /nobreak
 call :getTime now
 if "%now%" geq "13:05:00,00" ( goto :eof )
